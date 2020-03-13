9931ada5f3058d2f115fe503f705db35ca7b2968
PREACHER <44905222+PRE4CHER@users.noreply.github.com>
2020-03-13 19:26:17 +0300

    ConsoleApp original

---
+++ NTierApp.BLL/Models/CompanyModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace NTierApp.BLL.Models
+{
+    public class CompanyModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Adress { get; set; }
+        public ICollection<EmployeeModel> Employees { get; set; }
+    }
+}
---
+++ NTierApp.PL/Controllers/EmployeeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NTierApp.BLL.Interfaces;
+using NTierApp.PL.Models;
+
+namespace NTierApp.PL.Controllers
+{
+    public class EmployeeController : Controller
+    {/*
+        EmployeeService db = new EmployeeService();
+        static EmployeeController()
+        {
+
+        }
+        // GET: Employee
+        */
+        //private readonly Class1 class1; //
+        private readonly IEmployeeService _employeeService;
+        
+        public EmployeeController(IEmployeeService employeeService)//, Class1 class1)
+        {
+            //var container = AutofacConfig.ConfigureContainer("DefaultConnection");
+            //_employeeService = container.Resolve(typeof(IEmployeeService)) as IEmployeeService;
+            //var x = class1.Get5();
+            _employeeService = employeeService;            
+        }
+
+        public ActionResult Index(int companyId)
+        {
+            var employees = _employeeService.GetAllEmloyees();
+
+            return View();
+        }
+    }
+}
